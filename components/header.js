9535ee859277894495669c7718b858258e72d0b5
MisaelSalazar <misaelsalazarof@protonmail.com>
2024-10-23 21:09:09 -0600

    Prueba componentes

--- components/header.js
+++ components/header.js
@@ -1,9 +1,22 @@
-function header(){
+function navbar(content){
 
-    return(
-    
-        <h1>Hola Mundo</h1>
+    document.write(
+        "<nav class = 'navbar is-success p-6'>"+
+            
+            content+
 
+        "</nav>"
     )
 
 }
+function header(){
+
+    return (
+        
+        navbar(
+            "<h1 class='title has-text-centered'>Misael Salzar</h1>"
+        )
+    )
+
+}
+header()
